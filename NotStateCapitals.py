c2d96468c77f8be40a59bba8df13ed781a03b047
HallidayKa <54856896+HallidayKa@users.noreply.github.com>
2019-12-05 16:47:34 -0600

    initial commit

---
+++ NotStateCapitals.py
@@ -0,0 +1,25 @@
+import random
+# Quiz stuff, weird facts per state
+facts = {'Alabama': 'Actually has a town called Sweet Home', 'Alaska': 'Produce here is abnormally large', 'Arizona': 'It is illegal to cut down a specific type of cactus',
+				'Arkansas': 'Is home to one of the largest gems in America', 'California': 'It is illegal to bury the dead in San Fransisco. Presumably it is also illegal to bury the living',
+				'Colorado': 'The only state to turn down the Olympics', 'Conneticut': 'The birthplace of the hamburger', 'Delaware': 'Bob Marley lived here for a bit. No, really.', 'Florida': 'There are, unfortunately, alligators and crocodiles here',
+				'Georgia': 'It has an official state possum', 'Hawaii': 'This state has an official language', 'Idaho': 'Has the only state seal designed by a woman',
+				'Illinoise': 'The only state on this list I have purposefully misspelled', 'Indiana': 'Popcorn capital of the world', 'Iowa': 'A lot of mammoths died here. Not like recently or anything',
+				'Kansas': 'Home to the biggest ball of twine in the world', 'Kentucky': 'Was originally part of Virginia', 'Louisiana': 'This is where the turducken was born',
+				'Maine': 'It has a desert', 'Maryland': 'Home of the Ouija board', 'Massachusetts': 'It is illegal to frighten pigeons from their homes',
+				'Michigan': 'This is the magic capital of the world', 'Minnesota': 'Home to a tri-flowing river', 'Mississippi': 'Citizens here go to church more than anybody else in America',
+				'Missouri': 'Home to the deadliest tornado in the America', 'Montana': 'Much of the land here is federally owned', 'Nebraska': 'Birthplace of Kool-Aid',
+				'Nevada': 'Capitol of nuclear testing.', 'New Hampshire': 'Home to Mystery Hill', 'New Jersey': 'It has a volcano', 'New Mexico': 'Highest capital in the US',
+				'New York': 'Home to a subway that is mere hours away from flooding at any given moment', 'North Carolina': 'Home to the most American Idol finalists',
+				'North Dakota': 'Pharmacies are required to mostly be owned by local pharmacists', 'Ohio': 'Only US flag that is not a rectangle', 'Oklahoma': 'Cow chip throwing capital of the world',
+				'Oregon': 'Home to the largest living organism', 'Pennsylvania': 'Home to the raven that inspired the poem by Edgar Allen Poe', 'Rhode Island': 'Smallest state in the US',
+				'South Carolina': 'Home to Monkey Island', 'South Dakota': 'Fake center of America', 'Tennessee': 'Where Jack Daniels died', 'Texas': 'You can legally tag a Bigfoot here',
+				'Utah': 'Home to a lot of Jello lovers', 'Vermont': 'Lest religious state in America', 'Virginia': 'Vanity plate capital of America',
+				'Washington': 'Has more glaciers than any other state', 'West Virginia': 'Home to Greenbrier Hotel, where many presidents have stayed', 'Wisconsin': 'The state bird is actually fake',
+				'Wyoming': 'Only has two escalators in the entire state'}
+# Generate 35 quiz files.
+for quizNum in range(35):
+ # TODO: Create the quiz and answer key files.
+ # TODO: Write out the header for the quiz.
+ # TODO: Shuffle the order of the states.
+ # TODO: Loop through all 50 states, making a question for each.
